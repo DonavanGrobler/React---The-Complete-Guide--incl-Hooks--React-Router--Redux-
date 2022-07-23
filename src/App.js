69661de908a619be15a38be8822ecc33ac48cb56
Donavan Grobler <donavan.grobler@entelect.co.za>
2022-07-23 23:05:23 +0200

    Finished section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -25,32 +25,12 @@ function App() {
       title: "Tooth Paste",
       amount: 29.99,
       date: new Date(2025, 2, 14),
-    },
+    }
   ];
-
   return (
     <div>
       <h2>My Blog Page</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
